71d4f904664b31e62e3fad0fc160ae239904f7e9
bennett2oakley-hue <bennett2oakley@gmail.com>
2026-09-24 21:52:55 -0400

    Refresh offline cache for upgraded Hotel Easy Pass

--- sw.js
+++ sw.js
@@ -1,40 +1,27 @@
-const CACHE_NAME = "hotel-easy-pass-v2";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./main.js",
-  "./manifest.webmanifest"
-];
+const CACHE_NAME = "hotel-easy-pass-v3";
+const FILES_TO_CACHE = ["./", "./index.html", "./main.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }).catch(() => cached))
   );
 });
